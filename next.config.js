bc651e233e9b96a3361cce7d06d5ea4b21bf19f6
JTSanthosh0007 <djshettydjshetty@gmail.com>
2025-06-05 09:07:12 +0530

    Add Content-Security-Policy to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     forceSwcTransforms: true // Force SWC transforms
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; connect-src 'self' https://app-sicb.onrender.com; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; media-src 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self';`
+          },
+        ]
+      },
+    ];
   }
 }
 
